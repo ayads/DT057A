e8f9d303a97cd34b579aa8f553c02697baf4f736
Amanda Ericson <38975130+amandaericson@users.noreply.github.com>
2020-12-15 15:33:33 +0100

    Updated part1 with latest code

--- part1.cc
+++ part1.cc
@@ -3,7 +3,6 @@
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
-#include <iomanip>
 #include <random>
 #include <fstream>
 #include <map>
@@ -14,14 +13,17 @@
 #include <numeric>
 
 double factorial(int n);
-void poissonDist(int lambda, int size);
-void expDist(int lambda);
+void poissonDist(std::vector<long double> rand, int lambda, int size);
+void expDist();
 template<class T, int seed, int m, int a, int c> void LCG();
 
-std::ofstream LCGfile, UVfile, EXPfile, EXP_NS3file, POISSONfile;
-std::vector<long double> LCG_rand, exp_rand;
+std::ofstream LCGfile, UVfile;
+std::vector<long double> values;
 
 int main (int argc, char *argv[]){
+
+  /*****Exponential distribution*****/
+  //expDist();
 
   /*****Linear congurental engine implementation *****/
   LCGfile.open("outputLCG.txt");
@@ -30,33 +32,23 @@ int main (int argc, char *argv[]){
   LCG<std::uint_fast32_t, 1, 500, 3, 0>();
   LCGfile.close();
 
-  /*****Exponential distribution*****/
-  EXPfile.open("outputEXP.txt");
-  expDist(1);
-  EXPfile.close();
-
   /*****Poisson distribution*****/
-  POISSONfile.open("outputPOISSON.txt");
-  poissonDist(4, 10);
-  POISSONfile.close();
+  poissonDist(values, 4, 10);
 
   /*****UniformRandomVariable using ns3*****/
-  UVfile.open("outputUV.txt");
+  /*UVfile.open("outputUV.txt");
   ns3::Ptr<ns3::UniformRandomVariable> uv = ns3::CreateObject<ns3::UniformRandomVariable> ();
   for(int i=0; i<1000; i++){
     UVfile << std::setprecision(10) << uv->GetValue() << "\t" << i << "\n";
   }
-  UVfile.close();
+  UVfile.close();*/
 
   /*****ExponentialRandomVariable using ns3*****/
-  EXP_NS3file.open("outputNS3EXP.txt");
   ns3::Ptr<ns3::ExponentialRandomVariable> ERV = ns3::CreateObject<ns3::ExponentialRandomVariable> ();
-  //std::cout << "NS3 EXP:\n";
-  ERV->SetAttribute ("Bound", ns3::DoubleValue (1.0)); //[0,1.0] upper bound
-  for(int i=0; i<1000; i++){
-    EXP_NS3file << ERV ->GetValue() << "\n"; //<< "\t" << i
+  std::cout << "NS3 EXP:\n";
+  for(int i=0; i<9; i++){
+    std::cout << std::setprecision(10) << ERV ->GetValue() << " "; //<< "\t" << i
   }
-  EXP_NS3file.close();
   std::cout<<"\n";
   return 0;
 }
@@ -69,63 +61,97 @@ double factorial(int n)
         return 1;
 }
 
-void poissonDist(int lambda, int size){
+void poissonDist(std::vector<long double> rand, int lambda, int size){
+  //std::linear_congruential_engine<std::uint_fast32_t, 500, 3, 0> generator(1);
   std::vector<double> poisson, tr;
   std::vector<double> poissonEmp(size-1, 0.0);
 
- //Poisson fördelning
-  for(int i=0; i<size; i++){
+  for(int i=0; i<size-1; i++){
     poisson.push_back(pow(lambda,i+1)*exp(-lambda)*(1/factorial(i+1)));
   }
 
-  for(int i=size-1; i>=0; i--){
+  //for (auto i = poisson.begin(); i != poisson.end(); ++i) std::cout << *i << ' ';
+  //std::cout<< "\n";
+
+  for(int i=size-2; i>=0; i--){
     tr.push_back(std::accumulate(poisson.begin(), poisson.end()-i, 0.0));
   }
   //for (auto i = tr.begin(); i != tr.end(); ++i) std::cout << *i << ' ';
 
-  for(unsigned int i=0; i<LCG_rand.size(); i++){
-    for(int j=0; j<size; j++){
-      if(exp_rand[i]<tr[j]){
+  for(int i=0; i<rand.size(); i++){
+    for(int j=0; j<size-1; j++){
+      if(rand[i]<tr[j]){
         poissonEmp[j]++;
         break;
       }
     }
   }
 
-  for (int i=0; i<size-1; i++){
-    poissonEmp[i]= poissonEmp[i]/exp_rand.size();
+  for (int i=0; i<size-2; i++){
+    poissonEmp[i]= poissonEmp[i]/rand.size();
   }
+  std::cout << "POISSON:\n";
 
-  for (auto i = poissonEmp.begin(); i != poissonEmp.end(); ++i) POISSONfile << *i << '\n';
+  for (auto i = poissonEmp.begin(); i != poissonEmp.end(); ++i) std::cout << *i << ' ';
+  std::cout << "\n\n";
+  /*std::poisson_distribution<> distribution (1.0);
+
+  for (int i=0; i<10; ++i)
+    std::cout << i << "\t" << distribution(generator) << "\n";
+
+  std::cout << std::endl;*/
 }
 
-void expDist(int lambda){
+void expDist(){
+  std::linear_congruential_engine<std::uint_fast32_t, 500, 3, 0> gen(1);
 
-  for (int i = 0; i < 1000; ++i) {
-      exp_rand.push_back(lambda*exp((-lambda)*LCG_rand[i]));
+  std::exponential_distribution<> distr(1.0);
+  //std::cout <<std::setprecision(5) << distribution.lambda() << "\n";
+  std::cout << std::endl;
+  std::cout << "min() == " << distr.min() << std::endl;
+  std::cout << "max() == " << distr.max() << std::endl;
+  std::cout << "lambda() == " << std::fixed << std::setw(11) << std::setprecision(10) << distr.lambda() << std::endl;
+
+  // generate the distribution as a histogram
+  std::map<double, int> histogram;
+  for (int i = 0; i < 10; ++i) {
+      ++histogram[distr(gen)];
   }
 
-  for (auto i = exp_rand.begin(); i != exp_rand.end(); ++i) EXPfile << *i << "\n";
-
-  //std::cout << "\n";
+  // print results
+  std::cout << "Distribution for " << 10 << " samples:" << std::endl;
+  int counter = 0;
+  for (const auto& elem : histogram) {
+      std::cout << std::fixed << std::setw(11) << ++counter << ": "
+          << std::setw(14) << std::setprecision(10) << elem.first << std::endl;
+  }
+  std::cout << std::endl;
 }
 
 template<class T, int seed, int m, int a, int c>
 void LCG(){
+    //value = (seed*a+c)mod m
+    //std::linear_congruential_engine<T, m, a, c> E(seed);
+    //long double X = double(E())/E.max();
+
     long double X = ((seed*a) + c)%m;
-    LCG_rand.push_back(X);
+    //std::vector<long double> values;
+    values.push_back(X);
 
-    for(int i=0; i<1000; i++){
-      X = (((int)LCG_rand[i]*a) + c)%m;
-      LCG_rand.push_back(X);
+    for(int i=0; i<100; i++){
+      //X = double(E())/E.max();
+      X = (((int)values[i]*a) + c)%m;
+      values.push_back(X);
+      //std::cout << std::setprecision(10); //<< X << "\n";
+      //LCGfile << std::setprecision(10)<< X << "\n"; //"\t"<< i<<
     }
 
-    std::transform(LCG_rand.begin(), LCG_rand.end(), LCG_rand.begin(), [](long double& y){return y/m;});
+    std::transform(values.begin(), values.end(), values.begin(), [](long double& y){return y/m;});
 
-    int index=0;
-    for (auto i = LCG_rand.begin(); i != LCG_rand.end(); ++i){
-        LCGfile << *i << "\t" << index << "\n";
-        index++;
+    for (auto i = values.begin(); i != values.end(); ++i){
+      //std::cout << *i << ' ';
+      LCGfile << *i << "\n";
     }
+
 
 }
